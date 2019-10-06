22f2992cd6f228647a4cf862e5936e5a430b48b5
HaliimaR <rahimi.halima17@gmail.com>
2019-10-06 18:43:55 +0200

    count met color

--- src/App.js
+++ src/App.js
@@ -10,45 +10,40 @@ class App extends React.Component {
     super();
     this.state = {
       count: 0,
-      isLogged: false,
       color: ""
     };
     this.handleClick = this.handleClick.bind(this); //in handleClick de this kunt gebruiken moet je die binden
-    this.handleClick = this.handleClick.bind(this);
+
   }
   handleClick() {
     this.setState(prevState => {
-      return { count: prevState.count + 1 };
-    });
-  }
-  handletClick() {
-    this.setState(prevState => {
-      return {
-        isLogged: !prevState.isLogged
+      return { 
+        count: prevState.count + 1,
+        color: randomcolor()
       };
     });
   }
+  /*
   componentDidUpdate(prevState){
-    if(prevState.sount !== this.state.count)
+    if(prevState.count !== this.state.count)
     {
       const newColor = randomcolor();
-    this.setState({color: newColor});
+      this.setState({color: newColor});
     }
   }
+*/
   render() {
     let buttonText = this.state.isLogged ? "you klik" : "don't klik"; 
     return (
       <div className="App">
         <Header />
-        <header className="App-header">
           <div className="di">
             <h2 style={{color: this.state.color}}>{this.state.count}</h2> {/*javascript dubbel {*/}
-            <button onClick={this.handleClick} onClick={this.handletClick}> klik me </button>
+            <button onClick={this.handleClick}> klik me </button>
           </div>
 
           <MainContent />
           <Footer name="halima" />
-        </header>
       </div>
     );
   }
